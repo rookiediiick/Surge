a1abcd5dbdd566f88162a9b9411a9ef127a3e083
rookiediiick <56819681+rookiediiick@users.noreply.github.com>
2020-04-20 08:06:45 +0800

    Create YouTube.js

---
+++ Script/YouTube.js
@@ -0,0 +1,18 @@
+/*
+仅作为备份自用
+来源：https://github.com/Choler/Surge/blob/master/Script/YouTube.js
+
+使用方法：
+[Script]
+http-request ^https://[\s\S]*\.googlevideo\.com/.*&(oad|ctier) script-path=https://Choler.github.io/Surge/Script/YouTube.js
+[MITM]
+hostname = *.googlevideo.com
+*/
+
+var data = {
+  body: "{}",
+  headers: {
+    "Content-Type": "multipart/byteranges"
+  }
+};
+$done({response: data});
