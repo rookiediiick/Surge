49dfad2bba6196475b34435bc8bf44d70d4ae119
rookiediiick <56819681+rookiediiick@users.noreply.github.com>
2021-01-18 13:50:32 +0800

    Update Bilibiliauto.js

--- Script/Bilibiliauto.js
+++ Script/Bilibiliauto.js
@@ -1,25 +1,3 @@
-/*
-Bilibli番剧，自动切换地区
-
-此脚本仅适用于Surge4.0+ (iOS)
-此脚本仅适用于Surge4.0+ (iOS)
-此脚本仅适用于Surge4.0+ (iOS)
-
-您需要配置相关规则集（https://raw.githubusercontent.com/DivineEngine/Profiles/master/Surge/Ruleset/StreamingMedia/StreamingSE.list）绑定相关select策略组，并且需要具有相关的区域代理服务器纳入您的子策略中。
-最后，您可以通过BoxJs设置策略名和子策略名，或者手动填入脚本。
-
-Author: @NobyDa
-
-****************************
-Surge 4.2+ 远程脚本配置 :
-****************************
-[Script]
-Bili Region = type=http-response,pattern=https:\/\/api\.bilibili\.com\/pgc\/view\/(v\d\/)?app\/season\?access_key,requires-body=1,max-size=0,control-api=1,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bili_Auto_Regions.js
-
-[MITM]
-hostname = api.bilibili.com
-****************************
-*/
 
 const Group = $persistentStore.read('BiliArea_Policy') || 'StreamingSE'; //Your blibli policy group name.
 const CN = $persistentStore.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
